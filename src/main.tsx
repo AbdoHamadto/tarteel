2f3b9534af4524d5c558e5f4c7f812a0a3747bdf
AbdoArafh <abdoarafh54@gmail.com>
2025-04-03 18:09:36 +0200

    feat: improved sign-in style a bit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import '@fontsource-variable/rubik/index.css';
 import './index.css'
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
